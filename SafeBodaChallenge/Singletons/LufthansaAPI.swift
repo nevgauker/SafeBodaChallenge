70f26e75ddb4960e8dd92fffc0415a7731532eb2
rotem <nevgauker@gmail.com>
2019-09-25 19:12:41 +0200

    first commit of this project.

---
+++ SafeBodaChallenge/Singletons/LufthansaAPI.swift
@@ -0,0 +1,18 @@
+//
+//  LufthansaAPI.swift
+//  SafeBodaChallenge
+//
+//  Created by Rotem Nevgauker on 25/09/2019.
+//  Copyright © 2019 Rotem Nevgauker. All rights reserved.
+//
+
+import UIKit
+
+final class LufthansaAPI: NSObject {
+    
+    static let shared = LufthansaAPI()
+    
+    private override init() { }
+
+
+}
